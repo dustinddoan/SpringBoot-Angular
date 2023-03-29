6471631a34f3ae888b3ba31cc0a3e4ad302540b4
dustinddoan <dustin.ddoan@gmail.com>
2023-03-28 21:44:46 -0700

    add more Entiry on Spring Boot

---
+++ backend-springboot/src/main/java/com/noobdev/springbootecommerce/entity/Address.java
@@ -0,0 +1,33 @@
+package com.noobdev.springbootecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "address")
+@Data
+public class Address {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "street")
+  private String street;
+
+  @Column(name = "city")
+  private String city;
+
+  @Column(name = "state")
+  private String state;
+
+  @Column(name = "zip_code")
+  private String zipcode;
+
+  @Column(name = "country")
+  private String country;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @PrimaryKeyJoinColumn
+  private Order order;
+}
